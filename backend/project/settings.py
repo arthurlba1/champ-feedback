d2c18e623030848c524ba4dfd9c376ff1aeacfcc
arthurlba1 <1arthurluiz1@gmail.com>
2021-09-06 23:17:37 -0300

    feedbackChamp app added

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Implemented apps
+    'feedbackChamp',
+
 ]
 
 MIDDLEWARE = [
