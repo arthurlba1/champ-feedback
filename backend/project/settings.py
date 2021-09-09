8241540b948951696f2fe567bc6da2fe9f5d56bf
arthurlba1 <1arthurluiz1@gmail.com>
2021-09-09 00:33:24 -0300

    cors added to the project

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
     # Installed apps
     'rest_framework',
+    'corsheaders',
 
 ]
 
@@ -54,7 +55,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'project.urls'
 
