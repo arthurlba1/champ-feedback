d7e73002fcf56daa4c4b45c5c8ab0fd0e0c12a8d
arthurlba1 <1arthurluiz1@gmail.com>
2021-09-14 00:26:39 -0300

    NavBar style defined

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,15 +1,40 @@
 <template>
-    <div>
-        
+    <div class="navbar-container">
+        <h1>{{msg}}</h1>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'NavBar'
+    name: 'NavBar',
+    props: {
+        msg: String
+    }
 }
 </script>
 
 <style scoped>
+/*
+#D0A85C pastel
+#0BC6E3  blue
+#111111  black 
+*/
+
+.navbar-container {
+    position: absolute;
+    transition: 0.3s;
+    left: -80px;
+    padding: 15px;
+    width: 12vh;
+    text-decoration: none;
+    font-size: 20px;
+    color: white;
+    background-color:#111111;
+}
+
+.navbar-container:hover {
+    left: 0;
+}
+
 
 </style>
