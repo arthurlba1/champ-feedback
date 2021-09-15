fcd4d2c0ae4fa7d8e4a34c8f9b14934b100e05e3
arthurlba1 <1arthurluiz1@gmail.com>
2021-09-15 00:33:28 -0300

    getting all feedbacks through the component getfeedback

--- frontend/src/components/GetFeedback.vue
+++ frontend/src/components/GetFeedback.vue
@@ -1,12 +1,32 @@
 <template>
-    <div>
-        
-    </div>
+    <table class="table">
+        <thead>
+            <th>Name</th>
+            <th>Rating</th>
+            <th>Comment</th>
+        </thead>
+        <tbody>
+        <tr v-for="feedback in feedbacks" :key="feedback.id">
+            <th>{{ feedback.name }}</th>
+            <th>{{ feedback.rating }}</th>
+            <th>{{ feedback.comment }}</th>
+        </tr>
+        </tbody>
+    </table>
 </template>
 
 <script>
 export default {
-    name: 'GetFeedback'
+    name: 'GetFeedback',
+    data(){
+        return{
+            feedbacks: []
+        }
+    },
+    async created(){
+        var response = await fetch('http://127.0.0.1:8000/api/feedbackChamp/');
+        this.feedbacks = await response.json();
+    },
 }
 </script>
 
