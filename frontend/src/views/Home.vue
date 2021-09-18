eeeee96c975c9ccf7d03011d5d9d30d6632b247f
arthurlba1 <1arthurluiz1@gmail.com>
2021-09-17 21:25:30 -0300

    home page

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,73 @@
+<template>
+    <div id=home-container>
+        <NavBar/>
+        <div id="home-background-gradient"/>     
+        <div id="image-home"/>
+        <div id="image-effect-right"/>
+        <div id="image-effect-left"/>
+    </div>
+</template>
+
+<script>
+import NavBar from '@/components/NavBar'
+
+export default {
+    name: 'app',
+    components: {
+        NavBar
+    },
+}
+</script>
+
+<style>
+    #home-background-gradient{
+        height: 100vh;
+        background-image: linear-gradient(to right top, #181037, #18153e, #181b45, #18204c, #162653, #152a59, #142d5f, #123165, #11346c, #103873, #0e3b7a, #0c3f81);
+        position: absolute;
+        height: 100%;
+        width: 100%;
+        z-index: -5;
+    }
+    #home-container{
+        overflow: hidden;
+    }
+    #image-home{
+        background-image: url('C:/Users/arthu/OneDrive/Documents/Arthur/Projects/champ-feedback/frontend/src/assets/project-ashe-home-image.png');
+        background-repeat: no-repeat;
+        background-position: 100% 103%;
+        background-size: 135vh;
+        height: 100%;
+        width: 100%;
+        position: absolute;
+        z-index: 0;
+        opacity: 60%;
+        transform: scaleX(-1)
+    }
+    #image-effect-right{
+        position: absolute;
+        background-image: url('C:/Users/arthu/OneDrive/Documents/Arthur/Projects/champ-feedback/frontend/src/assets/oct-right.png');
+        z-index: -1;
+        right: 0%;
+        top: 0%;
+        height: 100%;
+        width: 100%;
+        background-size: 135vh;
+        background-position: 100% 85%;
+        background-repeat: no-repeat;
+        opacity: 12%;
+
+    }
+    #image-effect-left{
+        position: absolute;
+        background-image: url('C:/Users/arthu/OneDrive/Documents/Arthur/Projects/champ-feedback/frontend/src/assets/oct-left.png');
+        z-index: -1;
+        left: 0%;
+        bottom: 0%;
+        height: 100%;
+        width: 100%;
+        background-size: 135vh;
+        background-position: -3% 85%;
+        background-repeat: no-repeat;
+        opacity: 9%;
+    }
+</style>
